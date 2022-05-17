68327340f88a47e2b9f2d0ce5373add1924e54b4
Konul R. Huseynli <konulrh@code.local>
2022-05-17 18:41:41 +0400

    addednewtak

---
+++ task1methodstring.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Konul Mirsaleh Aysun";
+
+
+            char[] arr;
+            arr = name.ToCharArray();
+
+
+            {
+                for (int i = arr.Length - 1; i >= 0; i--)
+                {
+                    Console.WriteLine(arr[i]); ;
+
+                }
+            }
+        }
+    }
+}
